48bda202e34b7b3aa9a6e22284f23a759f9a79e6
Feng Wu <wufeng1740@gmail.com>
2024-06-24 21:06:46 +0930

    Initial Commit

---
+++ Study Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Study Timer
+//
+//  Created by 吴峰 on 24/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Study Timer/Study_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_TimerApp.swift
+//  Study Timer
+//
+//  Created by 吴峰 on 24/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct Study_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
